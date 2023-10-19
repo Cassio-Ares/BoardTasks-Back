e072441a6c93aec04651c7c48f710dc2b2c76c3e
Cassio-Ares <137903978+Cassio-Ares@users.noreply.github.com>
2023-10-19 09:45:45 -0300

    readme

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,8 @@
+/**
+ * arquivo onde são configurados end-point (rotas)
+ * nos tiramos isso do index.js e trazemos para cá
+ */
+
 function routes(app){
     app.use('/users', require('./routes/users.js'));
     return;
