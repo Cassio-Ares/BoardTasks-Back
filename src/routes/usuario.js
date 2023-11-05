d3f29141a620ca250bde5ee953371bbcb4cae811
Cassio-Ares <137903978+Cassio-Ares@users.noreply.github.com>
2023-11-05 15:59:13 -0300

    rota de login

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 const conectarBancoDados = require('../middlewares/conectarBD');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const EsquemaUsuario = require('../models/usuario');
-const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 const router = express.Router();
-
 
 
 router.post('/criar', conectarBancoDados, async function(req, res) {
@@ -20,13 +20,47 @@ router.post('/criar', conectarBancoDados, async function(req, res) {
       statusMensagem: "Usuário criado com sucesso.",
       resposta: respostaBD
     })
-  
- }catch (error) {
-  if(String(error).includes("email_1 dup key")){
-    return tratarErrosEsperados(res, "Error: Já existe uma conta com esse e-mail!");
+
+  } catch (error) {
+    if(String(error).includes("email_1 dup key")){
+      return tratarErrosEsperados(res, "Error: Já existe uma conta com esse e-mail!");
+    }
+    return tratarErrosEsperados(res, error);
   }
-  return tratarErrosEsperados(res, error);
-}
 });
+
+
+
+router.post('/logar', conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { email, senha } = req.body;
+
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select('+senha');
+    if (respostaBD) {
+
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        
+        let token = jwt.sign({ id: respostaBD._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+        res.header('x-auth-token', token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso.",
+          resposta: { "x-auth-token": token }
+        });
+      } else {
+        throw new Error("Email ou senha incorreta");
+      }
+    } else {
+      throw new Error("Email ou senha incorreta");
+    }
+  } catch (err) {
+    return tratarErrosEsperados(res, err);
+  }
+});
+
+
 
 module.exports = router;
